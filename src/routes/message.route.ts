2cf2d08aadca31a333c895e06dd88df89fe490fa
Art San <artsan.mcc@gmail.com>
2024-07-17 18:29:20 +0700

    add messages

--- src/routes/message.route.ts
+++ src/routes/message.route.ts
@@ -1,25 +1,14 @@
 import express from 'express'
-
+import protectRoute from '../middleware/protectRoute.js'
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage
+} from '../controllers/message.controller.js'
 const router = express.Router()
 
-router.get('/conversations', (req, res) => {
-  res.send('conversations')
-})
-router.get('/:id', (req, res) => {
-  res.send('id')
-})
-router.post('/send/:id', (req, res) => {
-  res.send('/send/id')
-})
+router.get('/conversations', protectRoute, getUsersForSidebar)
+router.get('/:id', protectRoute, getMessages) // этот роут должен быть ниже чем /conversations, а то перебивает
+router.post('/send/:id', protectRoute, sendMessage)
 
 export default router
-// import express from "express";
-// import protectRoute from "../middleware/protectRoute.js";
-// import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
-// const router = express.Router();
-
-// router.get("/conversations", protectRoute, getUsersForSidebar);
-// router.get("/:id", protectRoute, getMessages);
-// router.post("/send/:id", protectRoute, sendMessage);
-
-// export default router;
