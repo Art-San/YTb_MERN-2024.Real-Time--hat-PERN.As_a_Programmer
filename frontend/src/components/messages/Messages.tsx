fca35b643ac9b247c2805d3aff627357573af2a2
Art San <artsan.mcc@gmail.com>
2024-07-18 18:36:10 +0700

    ok

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,25 +1,44 @@
-import useChatScroll from "../../hooks/useChatScroll";
-import useGetMessages from "../../hooks/useGetMessages";
-import useListenMessages from "../../hooks/useListenMessages";
-import MessageSkeleton from "../skeletons/MessageSkeleton";
-import Message from "./Message";
+import { DUMMY_MESSAGES } from '../../_ui_design/dummy_data/dummy'
+import Message from './Message'
 
 const Messages = () => {
-	const { loading, messages } = useGetMessages();
-	useListenMessages();
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      {DUMMY_MESSAGES.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
+    </div>
+  )
+}
+export default Messages
 
-	const ref = useChatScroll(messages) as React.MutableRefObject<HTMLDivElement>;
+// import useChatScroll from '../../hooks/useChatScroll'
+// import useGetMessages from '../../hooks/useGetMessages'
+// import useListenMessages from '../../hooks/useListenMessages'
+// import MessageSkeleton from '../skeletons/MessageSkeleton'
+// import Message from './Message'
 
-	return (
-		<div className='px-4 flex-1 overflow-auto' ref={ref}>
-			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+// const Messages = () => {
+//   const { loading, messages } = useGetMessages()
+//   useListenMessages()
 
-			{!loading && messages.map((message) => <Message key={message.id} message={message} />)}
+//   const ref = useChatScroll(messages) as React.MutableRefObject<HTMLDivElement>
 
-			{!loading && messages.length === 0 && (
-				<p className='text-center text-white'>Send a message to start the conversation</p>
-			)}
-		</div>
-	);
-};
-export default Messages;
+//   return (
+//     <div className="px-4 flex-1 overflow-auto" ref={ref}>
+//       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+//       {!loading &&
+//         messages.map((message) => (
+//           <Message key={message.id} message={message} />
+//         ))}
+
+//       {!loading && messages.length === 0 && (
+//         <p className="text-center text-white">
+//           Send a message to start the conversation
+//         </p>
+//       )}
+//     </div>
+//   )
+// }
+// export default Messages
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="flex h-[80vh] w-full md:max-w-screen-md md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
-      {/* <MessageContainer /> */}
+      <MessageContainer />
     </div>
   )
 }
