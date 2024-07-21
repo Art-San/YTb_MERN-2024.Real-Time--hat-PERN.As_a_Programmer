31b91ed60d7d2d44b3e774e1d072370a73a926cf
Art San <artsan.mcc@gmail.com>
2024-07-22 00:23:03 +0700

    ok

---
+++ frontend/src/types/global.d.ts
@@ -0,0 +1,7 @@
+// global.d.ts — это специальный файл. Добавленные здесь типы можно использовать глобально в проекте без их импорта.
+
+type ConversationType = {
+  id: string
+  fullName: string
+  profilePic: string
+}
